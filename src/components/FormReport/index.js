98a8d371ed3130578fcbc8eb16a06540a043e31f
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2021-04-13 21:20:15 -0300

    formulario de report

--- src/components/FormReport/index.js
+++ src/components/FormReport/index.js
@@ -1,97 +1,87 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, TextInput, Button, Text, TouchableOpacity } from 'react-native';
-import MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View, Button, Text, TouchableOpacity } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {
+  SafeArea, 
+  Container, 
+  ContainerHeader, 
+  Title, 
+  ContainerForm, 
+  InputGroup, 
+  TitleInput, 
+  DescriptionInput, 
+  ContainerAddress,
+  ViewAddressContainer,
+  EditAddressContainer,
+  ContainerButtons
+} from './styles';
 
 export default function FormReport() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.form}>
-      
-      <View>
-        <Text style={{fontWeight: "bold", textAlign: 'center', fontSize: 18, padding: 16}}>Reporte uma aglomeração!</Text>
-      </View>
-        
-        <View>
-          <View style={{borderBottomWidth: 1, borderColor: '#000', borderRadius: 5, marginBottom: 16}}>
-            <TextInput 
+    <SafeArea>
+      <Container>
+
+        <ContainerHeader>
+          <Title>Reporte uma aglomeração!</Title>
+        </ContainerHeader>
+
+        <ContainerForm>
+          <InputGroup>
+            <TitleInput
               placeholder='Título'
               errorStyle={{ color: 'red' }}
               errorMessage=''
               maxLength={50}
-              style={{minHeight: 40, padding: 4}}
             />
-          </View>
-          <View style={{borderBottomWidth: 1, borderColor: '#000', borderRadius: 5, marginBottom: 16}}>
-            <TextInput 
-              placeholder='Descrição' 
+          </InputGroup>
+          <InputGroup>
+            <DescriptionInput
+              placeholder='Descrição'
               errorStyle={{ color: 'red' }}
               errorMessage=''
-              multiline 
+              multiline
               maxLength={200}
-              style={{padding: 8}}
             />
-          </View>
-        </View>
+          </InputGroup>
+        </ContainerForm>
 
-        <View style={{flex: 0, flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'flex-start', minHeight: 80, marginBottom: 16, backgroundColor: '#f5f5f5', borderRadius: 5}}>
-          
-          <View style={{flex: 0.8, flexDirection: "row", justifyContent: 'center', alignItems: 'center', padding: 8}}>
-            
-            <View style={{flex: 0.1, padding: 4}}>
-              <MaterialCommunityIcons 
-                name="map-marker" 
-                size={28} color="#616161" 
-                type="solid" 
-                titleProps={{accessibilityHint: 'Ícone de endereço', accessibilityLabel:"Ícone de endereço", accessible:"true" }}
+        <ContainerAddress>
+          <ViewAddressContainer>
+            <View style={{ flex: 0.1, padding: 4 }}>
+              <MaterialCommunityIcons
+                name="map-marker"
+                size={28} color="#616161"
+                type="solid"
+                titleProps={{ accessibilityHint: 'Ícone de endereço', accessibilityLabel: "Ícone de endereço", accessible: "true" }}
               />
             </View>
 
-            <View style={{flex: 0.9, padding: 4}}>
-              <Text style={{textAlign: 'center'}}>Rua DASDADAS DSX, 147 - Passo D' Areia - Porto Alegre - xxxxxxxxxxxxxxxxxx</Text>
+            <View style={{ flex: 0.9, padding: 4 }}>
+              <Text selectable style={{ textAlign: 'center' }}>Rua DASDADAS DSX, 147 - Passo D' Areia - Porto Alegre - xxxxxxxxxxxxxxxxxx</Text>
             </View>
-            
-            
-          </View>
+          </ViewAddressContainer>
 
-          <View style={{flex: 0.2, flexDirection: "column", justifyContent: 'center', alignItems: 'center', padding: 8, borderLeftWidth: 1, borderLeftColor: '#c7c7c7'}}>
-            <TouchableOpacity 
+          <EditAddressContainer>
+            <TouchableOpacity
               onPress={(e) => console.log('Editar Endereço')}
             >
-              <MaterialCommunityIcons 
-                name="pencil-box-multiple" 
-                size={32} color="#616161" 
-                type="solid" 
-                titleProps={{accessibilityHint: 'Editar endereço', accessibilityLabel:"Editar endereço", accessible:"true" }}
+              <MaterialCommunityIcons
+                name="pencil-box-multiple"
+                size={32} color="#616161"
+                type="solid"
+                titleProps={{ accessibilityHint: 'Editar endereço', accessibilityLabel: "Editar endereço", accessible: "true" }}
               />
             </TouchableOpacity>
-          </View>
+          </EditAddressContainer>
+        </ContainerAddress>
 
-        </View>
+        <ContainerButtons>
+          <Button color={'#FF0000'} accessibilityLabel="Cancelar" title="Cancelar" onPress={(e) => console.log('Cancelar')} />
+          <Button color={'#00a830'} accessibilityLabel="Reportar" title="Reportar" onPress={(e) => console.log('Reportar')} />
+        </ContainerButtons>
 
-        <View style={styles.containerButtons}>
-          <Button style={styles.button} color={'#FF0000'} accessibilityLabel="Cancelar" title="Cancelar" onPress={(e) => console.log('Cancelar')}/>
-          <Button style={styles.button} color={'#00a830'} accessibilityLabel="Reportar" title="Reportar" onPress={(e) => console.log('Reportar')}/>
-        </View>
-      </View>
-    </SafeAreaView>
+      </Container>
+    </SafeArea>
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 16,
-  },
-  form: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  containerButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  button: {
-    padding: 20
-  }
-});
---
+++ src/components/FormReport/styles.js
@@ -0,0 +1,76 @@
+import { Animated, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+export const SafeArea = styled.SafeAreaView`
+  flex: 1;
+  background: #fff;
+`;
+
+export const Container = styled.View`
+  padding: 16px;
+`;
+
+export const ContainerHeader = styled.View`
+  padding: 16px;
+`;
+
+export const Title = styled.Text`
+  font-weight: bold;
+  text-align: center; 
+  font-size: 18px;
+  padding: 16px;
+`;
+
+export const ContainerForm = styled.View`
+  margin: 24px 0px 24px 0px
+`;
+
+export const InputGroup = styled.View`
+  border-bottom-width: 1px;
+  border-color:  #000;
+  border-radius: 5px;
+  margin-bottom: 16px;
+`;
+
+export const TitleInput = styled.TextInput`
+  padding: 8px;
+  min-height: 40px;
+`;
+
+export const DescriptionInput = styled.TextInput`
+  padding: 8px;
+`;
+
+export const ContainerAddress = styled.View`
+  flex: 0;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  min-height: 80px;
+  margin-bottom: 16px;
+  background: #f5f5f5;
+  border-radius: 5px;
+`;
+
+export const ViewAddressContainer = styled.View`
+  flex: 0.8;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+`;
+
+export const EditAddressContainer = styled.View`
+  flex: 0.2;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  border-left-width: 1px;
+  border-color: #c7c7c7;
+`;
+
+export const ContainerButtons = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
